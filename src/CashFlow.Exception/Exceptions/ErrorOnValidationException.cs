9e9852570f5acbaa53a87042ce86785e76dff2f8
Rafael <rbezerra874@gmail.com>
2025-11-18 09:58:02 +0000

    feat: add new messages to resources files

--- src/CashFlow.Exception/Exceptions/ErrorOnValidationException.cs
+++ src/CashFlow.Exception/Exceptions/ErrorOnValidationException.cs
@@ -3,17 +3,17 @@
 namespace CashFlow.Exception.Exceptions;
 public class ErrorOnValidationException : CashFlowException
 {
-    public List<string> Errors { get; set; }
+    private readonly List<string> _Errors;
 
     public override int StatusCode => (int)HttpStatusCode.BadRequest;
 
     public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
     {
-        Errors = errorMessages;
+        _Errors = errorMessages;
     }
 
     public override List<string> GetErrors()
     {
-        return Errors;
+        return _Errors;
     }
 }
